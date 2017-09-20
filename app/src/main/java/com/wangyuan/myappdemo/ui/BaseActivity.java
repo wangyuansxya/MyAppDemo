883391c06d54d6c8a06f4c1beb4e9ee9bbee67dd
wangyuansxya <wangyuansxya@163.com>
2017-09-20 18:30:03 +0800

    minor change

--- app/src/main/java/com/wangyuan/myappdemo/ui/BaseActivity.java
+++ app/src/main/java/com/wangyuan/myappdemo/ui/BaseActivity.java
@@ -6,8 +6,26 @@ import android.support.v4.app.FragmentActivity;
  * Created by wangyuan on 2017/9/20.
  */
 
-public class BaseActivity extends FragmentActivity {
+public class BaseActivity extends FragmentActivity implements ActivityWrapper {
 
 
+    @Override
+    public void initView() {
 
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void queryData() {
+
+    }
+
+    @Override
+    public void fillData() {
+
+    }
 }
