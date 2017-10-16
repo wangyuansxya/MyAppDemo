0069674b8509e6792d745290bec74defeae06fe4
wangyuan <wangyuan@51talk.com>
2017-10-16 13:42:23 +0800

    add OvalTransform

---
+++ app/src/main/java/com/wangyuan/myappdemo/image/transform/OvalTransform.java
@@ -0,0 +1,58 @@
+package com.wangyuan.myappdemo.image.transform;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+
+import com.squareup.picasso.Transformation;
+
+/**
+ * Created by wangyuan on 2017/10/16.
+ * 椭圆
+ */
+
+public class OvalTransform implements Transformation {
+
+    private Context c;
+    private int round;
+    public OvalTransform(Context c, int round){
+        this.c = c;
+        this.round = round;
+    }
+
+    private static Paint paint = new Paint();
+
+    @Override
+    public Bitmap transform(Bitmap source) {
+        int size = Math.min(source.getWidth(), source.getHeight());
+
+        int x = (source.getWidth() - size) / 2;
+        int y = (source.getHeight() - size) / 2;
+
+        Bitmap squaredBitmap = Bitmap.createBitmap(source, x, y, size, size);
+        if (squaredBitmap != source) {
+            source.recycle();
+        }
+        Bitmap bitmap = Bitmap.createBitmap(size, size, source.getConfig());
+        Canvas canvas = new Canvas(bitmap);
+
+        BitmapShader shader = new BitmapShader(squaredBitmap,
+                BitmapShader.TileMode.CLAMP, BitmapShader.TileMode.CLAMP);
+        paint.setShader(shader);
+        paint.setAntiAlias(true);
+        final int space = size / 32;
+        RectF rf = new RectF(0 + space, 0, size - space, size);
+        canvas.drawOval(rf, paint);
+
+        squaredBitmap.recycle();
+        return bitmap;
+    }
+
+    @Override
+    public String key() {
+        return "OvalTransform";
+    }
+}
